c9038f86235f5614333bb11c65339834737b584c
Programacion OpTicX <fdzk100@gmail.com>
2021-03-17 11:07:17 -0600

    first-commit

---
+++ 2020/Dia1/Parte1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> rompecabezas = new List<int>();
+            int numeros, resultado = 0;
+
+            //La lectura termina cuando tecleas 0
+            while (true)
+            {
+                numeros = Int32.Parse(Console.ReadLine());
+
+                if (numeros != 0) rompecabezas.Add(numeros);
+                else break;
+            }
+
+            //Recorremos la lista 2 veces hasta que los numeros sumen 2020
+            foreach (var item in rompecabezas)
+            {
+                foreach (var item2 in rompecabezas)
+                {
+                    if (item + item2 == 2020) resultado = item * item2;
+                }
+            }
+
+            //Imprimimos la multiplicacion
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ 2020/Dia1/Parte2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> rompecabezas = new List<int>();
+            int numeros, resultado = 0;
+
+            //La lectura termina cuando tecleas 0
+            while (true)
+            {
+                numeros = Int32.Parse(Console.ReadLine());
+
+                if (numeros != 0) rompecabezas.Add(numeros);
+                else break;
+            }
+
+            //Recorremos la lista 3 veces hasta que los numeros sumen 2020
+            foreach (var item in rompecabezas)
+            {
+                foreach (var item2 in rompecabezas)
+                {
+                    foreach (var item3 in rompecabezas)
+                    {
+                        if (item + item2 + item3 == 2020) resultado = item * item2 * item3;
+                    }
+                }
+            }
+
+            //Imprimimos la multiplicacion
+            Console.WriteLine(resultado);
+        }
+    }
+}
