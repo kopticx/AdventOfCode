dcaecd4acde925151af6581ec887806c98563fc9
unknown <fdzk100@gmail.com>
2021-07-29 13:14:26 -0500

    day5- 2015

---
+++ 2015/Dia5/Parte1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> rompecabezas = new List<string>();
+            char[] vocales = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            string[] subcadenasDenegadas = new string[] { "ab", "cd", "pq", "xy" };
+            string input;
+            int cadenasBuenas = 0;
+
+            Console.WriteLine("Ingresa tu rompecabezas, al terminar presiona 0");
+            while(true)
+            {
+                input = Console.ReadLine();
+
+                if (input != "0") rompecabezas.Add(input);
+                else break;
+            }
+
+            foreach (var item in rompecabezas)
+            {
+                int contadorVocales = 0;
+                bool letraSeguida = false, cadenaBuena = true;
+
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if(i + 1 <=  item.Length - 1)
+                    {
+                        if (item[i] == item[i + 1]) letraSeguida = true;
+
+                        if (Array.Exists(subcadenasDenegadas, x => x == item[i].ToString() + item[i + 1].ToString())) cadenaBuena = false;
+                    }
+
+                    if (Array.Exists(vocales, x => x == item[i])) contadorVocales++;
+                }
+
+                if (cadenaBuena && letraSeguida && contadorVocales >= 3) cadenasBuenas++;
+            }
+
+            Console.WriteLine("\nResultado:");
+            Console.WriteLine(cadenasBuenas);
+        }
+    }
+}
---
+++ 2015/Dia5/Parte2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> rompecabezas = new List<string>();
+            string input;
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input != "0") rompecabezas.Add(input);
+                else break;
+            }
+
+            Console.WriteLine("\nResultado:");
+            Console.WriteLine(PartTwo(rompecabezas));
+        }
+
+        public static object PartTwo(List<string> input) =>
+            input.Count(line => {
+                var appearsTwice = Enumerable.Range(0, line.Length - 1).Any(i => line.IndexOf(line.Substring(i, 2), i + 2) >= 0);
+                var repeats = Enumerable.Range(0, line.Length - 2).Any(i => line[i] == line[i + 2]);
+                return appearsTwice && repeats;
+            });
+    }
+}
